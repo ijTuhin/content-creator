dfaead0336cc3b2bd3b1fc2c415ae47f15da7185
ijTuhin <ijTuhin638@gmail.com>
2022-10-06 22:29:47 -0700

    tailwindCSS installed | new comp added

---
+++ src/Components/DndFiles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DndFiles = () => {
+    return (
+        <div className='flex justify-center m-10'>
+            <div className='w-96 border p-5 bg-sky-100'>
+                <h3 className='text-2xl text-center text-gray-600 font-medium'>Upload or Drag Your File</h3>
+            </div>
+        </div>
+    );
+};
+
+export default DndFiles;
