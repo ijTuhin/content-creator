eeeb506d1da377b3aba51bbeba3b940e88b1b739
ijTuhin <ijTuhin638@gmail.com>
2022-10-07 11:10:47 -0700

    react-drag-drop-file installed but showing error

--- src/Components/DndFiles.js
+++ src/Components/DndFiles.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+
+const fileTypes = ["JPG", "PNG", "GIF"];
 
 const DndFiles = () => {
+    const [file, setFile] = useState([]);
+    const handleChange = (file) => {
+        setFile(file);
+    };
+
     return (
-        <div className='flex justify-center m-10'>
-            <div className='w-96 border p-5 bg-sky-100'>
-                <h3 className='text-2xl text-center text-gray-600 font-medium'>Upload or Drag Your File</h3>
+        <div className='flex flex-col items-center m-10'>
+            <div className='w-96 border p-5 mb-10 bg-sky-100'>
+                <h3 className='text-2xl text-center text-gray-600 font-medium mb-5'>Upload or Drag Your File</h3>
+                <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
             </div>
+
+            <p>Your File:</p>
+            {file}
         </div>
     );
 };
